6a112f3df9bd41971827ee8f754ae035d535854a
alig06 <ali.gkts.14@gmail.com>
2018-09-11 10:44:09 +0300

    Dashboard

--- integration/UAT-Dashboard.js
+++ integration/UAT-Dashboard.js
@@ -1,7 +1,9 @@
 describe('Dashboard',()=>{
 	before('LOGİN',()=>{
 		//LOGİN
-		cy.visit('www.prisync.com/login')
+		cy.clearCookies()
+		cy.wait(1500)
+		cy.visit('https://www.prisync.com/login')
 		cy.get('[name="email"]').type('TryAccount@hotmail.com')
 		cy.get('[name="password"]').type('Pri_Ui=05')
 		cy.get('button[name="submit"]').contains('Sign In').click()
@@ -10,58 +12,63 @@ describe('Dashboard',()=>{
 		
 		//Take to numbers
 		cy.get('a').contains('Products').click()
-		cy.get('[class="brand-name"]').then(numberLen => {
+		cy.get('[class="brand-name"]',{timeout:15000}).then(numberLen => {
 			var numberBrand = numberLen.length
 			console.log(numberBrand)
-			cy.get('[class="category-name"]').then(categoryLen=>{
+			cy.get('[class="category-name"]',{timeout:15000}).then(categoryLen=>{
 				var category = categoryLen.length
 				console.log(category)
-				cy.get('[class="product"]').then(productLen=>{
+				cy.get('[class="product"]',{timeout:15000}).then(productLen=>{
 					var product = productLen.length
 					console.log(product)
 
 					// Controlling to numbers equal
-					cy.get('a').contains('Dashboard').click()
-					cy.get('[href="/panel"]').find('.num-summary').contains(product)
-					cy.get('[href="/panel/report/type/brand"]').find('.num-summary').contains(numberBrand)
-					cy.get('[href="/panel/report/type/category"]').find('.num-summary').contains(category)
+					cy.get('a',{timeout:15000}).contains('Dashboard').click()
+					cy.get('[href="/panel"]',{timeout:15000}).find('.num-summary').contains(product)
+					cy.get('[href="/panel/report/type/brand"]',{timeout:15000}).find('.num-summary').contains(numberBrand)
+					cy.get('[href="/panel/report/type/category"]',{timeout:15000}).find('.num-summary').contains(category)
 				})
 			})
 		})
 		//Index Text
-		cy.get('a').contains('Dashboard').click()
-		cy.get('[id="myspartkline"]',{timeout:15000}).contains('Updating indices...')
+		cy.get('a',{timeout:15000}).contains('Dashboard').click()
+		cy.wait(5000)
+		cy.get('[id="index-info"]',{timeout:15000}).find('.text-center').then(ln=>{
+			var len = ln.val().length
+			if (len == 0 || len >= 1){
+				//Price Changes
+				cy.get('h2').contains('Price changes').parent().should('be.visible')
+				
+				//If this field's text equal to 0 , Passed .
+				cy.get('[id="increased"]',{timeout:15000}).should('have.text','0')
+				cy.get('[id="decreased"]',{timeout:15000}).should('have.text','0')
 
-		//--------------------------------------------------------------------------------
+				//--------------------------------------------------------------------------------
 
-		//Price Changes
-		cy.get('h2').contains('Price changes').parent().should('be.visible')
-		
-		//If this field's text equal to 0 , Passed .
-		cy.get('[id="increased"]').should('have.text','0')
-		cy.get('[id="decreased"]').should('have.text','0')
+				//Out of stock
+				cy.get('h2',{timeout:15000}).contains('Out of stock').parent().should('be.visible')
 
-		//--------------------------------------------------------------------------------
+				//If this field's text equal to 0 , Passed .
+				cy.get('[id="competitorstockout"]',{timeout:15000}).should('have.text','0')
+				cy.get('[id="mystockout"]',{timeout:15000}).should('have.text','0')
 
-		//Out of stock
-		cy.get('h2').contains('Out of stock').parent().should('be.visible')
+				//--------------------------------------------------------------------------------
 
-		//If this field's text equal to 0 , Passed .
-		cy.get('[id="competitorstockout"]').should('have.text','0')
-		cy.get('[id="mystockout"]').should('have.text','0')
+				//Position
+				cy.get('h2',{timeout:15000}).contains('Position').parent().should('be.visible')
+				
+				//If this field's text equal to 0 , Passed .
+				cy.get('[id="cheapest"]',{timeout:15000}).should('have.text','0')
+				cy.get('[id="cheaper"]',{timeout:15000}).should('have.text','0')
+				cy.get('[id="average"]',{timeout:15000}).should('have.text','0')
+				cy.get('[id="higher"]',{timeout:15000}).should('have.text','0')
+				cy.get('[id="highest"]',{timeout:15000}).should('have.text','0')
+				cy.get('[id="allequal"]',{timeout:15000}).should('have.text','0')
 
-		//--------------------------------------------------------------------------------
+			}
+			
 
-		//Position
-		cy.get('h2').contains('Position').parent().should('be.visible')
-		
-		//If this field's text equal to 0 , Passed .
-		cy.get('[id="cheapest"]').should('have.text','0')
-		cy.get('[id="cheaper"]').should('have.text','0')
-		cy.get('[id="average"]').should('have.text','0')
-		cy.get('[id="higher"]').should('have.text','0')
-		cy.get('[id="highest"]').should('have.text','0')
-		cy.get('[id="allequal"]').should('have.text','0')
+		})
 
 	})
 })
